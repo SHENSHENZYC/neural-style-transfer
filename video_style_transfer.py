697c25064766b730fcf3b819bf4e2034d2beb596
SHENSHENZYC <alexyczhao@gmail.com>
2023-06-13 19:07:53 -0700

    update video style transfer python file

--- video_style_transfer.py
+++ video_style_transfer.py
@@ -130,10 +130,11 @@ def video_style_transfer(config):
     style_img_name, _ = get_image_name_ext(style_path)
     output_video_path = os.path.join(output_dir, f"nst-{content_video_name}-{style_img_name}-final.mp4")
 
-    frame_height, frame_width, _ = cv2.imread(os.path.join(output_dir, "transferred_frames", "transferred_frame-00000001.jpg")).shape
+    output_frame_height, output_frame_width, _ = cv2.imread(os.path.join(output_dir, "transferred_frames", "transferred_frame-00000001.jpg")).shape
+    output_fps = config.get('fps') if config.get('fps') is not None else fps
     # synthesize video using transferred content frames
     cv2_fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    video_writer = cv2.VideoWriter(output_video_path, cv2_fourcc, fps, (frame_width, frame_height), True)
+    video_writer = cv2.VideoWriter(output_video_path, cv2_fourcc, output_fps, (output_frame_width, output_frame_height), True)
 
     for i in range(total_frames):
         frame = cv2.imread(os.path.join(output_dir, "transferred_frames", f"transferred_frame-{i+1:08d}.jpg"))
@@ -156,6 +157,7 @@ def main():
     parser.add_argument("--style_filepath", required="--file_dir" not in sys.argv, type=str, help="Path to the style image if image_dir not provided.")
     parser.add_argument("--output_dir", required="--file_dir" not in sys.argv, type=str, help="Directory that stores the output video. Will be the same as file_dir if not provided while image_dir provided.")
     parser.add_argument("--output_frame_size", nargs="+", type=int, help="Size of frames of output video. Either one integer or two integers (height, weight) separated by space is accepted. Will use the dimensions of frames of content video if not provided.")
+    parser.add_argument("--fps", type=int, help="FPS of output video. Will use the FPS of content video if not provided.")
     parser.add_argument("--init_method", choices=["content", "random"], default="content", help="Initialization method of generated image. Either \"random\" by randomization or \"content\" based on content image (recommended). Default is \"content\".")
     parser.add_argument("--quiet", type=bool, default=False, help="True stops showing debugging messages, loss function values during training process, and stops generating intermediate images.")
 
